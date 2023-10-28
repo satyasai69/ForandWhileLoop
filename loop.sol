653b255deacf89c511e4a8efdd335bb0947d3bbd
satyasai69 <ch.satya.sai133@gmail.com>
2023-10-28 15:31:25 +0530

    loop function

--- loop.sol
+++ loop.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.19;
 
 contract Loop {
-    
+
+    function loop () public pure {
+        for (uint i=0; i< 10; i++) {
+            if ( i == 3 ) {
+                continue;
+            }
+            if(i == 5) {
+                break;
+            }
+        }
+    }
 }
